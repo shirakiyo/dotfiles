77dbc3f07f8a659071a9cfac7aaaac723c785692
shirakiyo <tus.sk5678@gmail.com>
2025-06-01 16:44:16 +0900

    add install-zsh-plugin.sh

---
+++ scripts/install-zsh-plugin.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
+
